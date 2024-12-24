ea28c5e9aa3f996302a0dcac9c3a7ba11ff2490c
Mark Legkovskis <55879905+MLegkovskis@users.noreply.github.com>
2024-12-24 21:37:35 +0100

    attach iam

--- main.tf
+++ main.tf
@@ -16,7 +16,20 @@ provider "google" {
   region  = "europe-west2"
 }
 
-# VPC & Firewall
+##################################################
+# 1) Grant the SA "iam.serviceAccountUser" role
+##################################################
+# This ensures that the service account can be attached
+# to the compute instance by Terraform.
+resource "google_project_iam_member" "grant_sa_user_role" {
+  project = "fresh-circle-431620-r4"
+  role    = "roles/iam.serviceAccountUser"
+  member  = "serviceAccount:demo-cv@fresh-circle-431620-r4.iam.gserviceaccount.com"
+}
+
+##################################################
+# 2) VPC & Firewall (All Ports Open for Demo)
+##################################################
 resource "google_compute_network" "vpc_demo" {
   name                    = "vpc-demo"
   auto_create_subnetworks = true
@@ -34,6 +47,9 @@ resource "google_compute_firewall" "demo_allow_all" {
   source_ranges = ["0.0.0.0/0"]
 }
 
+##################################################
+# 3) Compute Instance (OS Login + SA attached)
+##################################################
 resource "google_compute_instance" "demo_vm" {
   name         = "demo-vm"
   machine_type = "e2-micro"
@@ -50,17 +66,27 @@ resource "google_compute_instance" "demo_vm" {
     access_config {}
   }
 
+  # Enable OS Login
   metadata = {
     enable-oslogin = "TRUE"
   }
 
-  # Hardcode the SA email
+  # Attach the same SA. Must have roles/iam.serviceAccountUser 
+  # via google_project_iam_member above
   service_account {
     email  = "demo-cv@fresh-circle-431620-r4.iam.gserviceaccount.com"
     scopes = ["cloud-platform"]
   }
+
+  # Wait for the IAM role to be granted before creation
+  depends_on = [
+    google_project_iam_member.grant_sa_user_role
+  ]
 }
 
+##################################################
+# 4) Public IP Output
+##################################################
 output "public_ip" {
   description = "The public IP of the ephemeral VM"
   value       = google_compute_instance.demo_vm.network_interface[0].access_config[0].nat_ip
