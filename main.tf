88e28bbebe04b90f8086a857c75c106e4e64465e
Mark Legkovskis <55879905+MLegkovskis@users.noreply.github.com>
2024-12-24 10:49:42 +0100

    add ansible

--- main.tf
+++ main.tf
@@ -14,35 +14,34 @@ provider "google" {
 }
 
 ###################################
-# 1) VPC + Firewall
+# VPC + Firewall (all ports open)
 ###################################
 resource "google_compute_network" "vpc_demo" {
   name                    = "vpc-demo"
   auto_create_subnetworks = true
 }
 
-# DEMO only: opens ALL ports from anywhere
 resource "google_compute_firewall" "demo_allow_all" {
   name    = "demo-allow-all"
   network = google_compute_network.vpc_demo.self_link
 
   allow {
     protocol = "tcp"
-    # All TCP ports
-    ports = ["0-65535"]
+    ports    = ["0-65535"]
   }
 
   source_ranges = ["0.0.0.0/0"]
 }
 
 ###################################
-# 2) Compute Instance
+# Compute Instance (no startup script)
 ###################################
 resource "google_compute_instance" "demo_vm" {
   name         = "demo-vm"
   machine_type = "e2-micro"
   zone         = "europe-west2-a"
 
+  # Boot disk with Debian
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
@@ -55,27 +54,16 @@ resource "google_compute_instance" "demo_vm" {
     access_config {}
   }
 
-  # The startup script references <<TAG_REPLACE>> or var.docker_tag
-  metadata_startup_script = <<-EOF
-    #!/bin/bash
-    apt-get update
-    apt-get install -y apt-transport-https ca-certificates curl gnupg
-    
-    # Add Docker's official GPG key
-    curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-    echo "deb [arch=amd64] https://download.docker.com/linux/debian buster stable" >> /etc/apt/sources.list
-
-    apt-get update
-    apt-get install -y docker-ce docker-ce-cli containerd.io
-
-    # Example with placeholder. Will be replaced by sed or var
-    docker pull marilee/devops:<<TAG_REPLACE>>
-    docker run -d -p 8080:8080 --name java-app marilee/devops:<<TAG_REPLACE>>
-  EOF
+  # Insert your SSH public key so you can SSH in via Ansible
+  # This is one approach: use instance metadata "ssh-keys" to embed your key
+  # Format is: "USERNAME:SSH_PUBLIC_KEY"
+  metadata = {
+    ssh-keys = "root:${file("./id_rsa.pub")}"
+  }
 }
 
 ###################################
-# 3) Output the IP
+# Output the public IP
 ###################################
 output "public_ip" {
   description = "The public IP of the ephemeral VM"
